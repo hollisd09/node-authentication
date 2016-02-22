9595b426185878fbbf5917253d7cd06e38ba9063
hollisd09 <hollisd09@gmail.com>
2016-02-22 11:18:27 -0600

    Added Redis

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  if (req.body.password === req.body.verify) {
+    res.redirect('/login');
+  } else {
+    res.render('register', {
+      email: req.body.email,
+      message: 'Passwords do not match'
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
