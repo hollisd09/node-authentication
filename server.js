bce206c8758baa42cad16bf2f3ec7eb578477d64
hollisd09 <hollisd09@gmail.com>
2016-02-23 10:42:44 -0600

    Added Passport

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const methodOverride = require('methodOverride');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
@@ -15,16 +16,17 @@ const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 app.use(session({
   secret: SESSION_SECRET,
   store: new RedisStore()
 }));
 app.use(userRoutes);
 
-res.locals.title = '';
+app.locals.title = '';
 
 app.use((req, res, next) => {
-  app.locals.user = req.session.user || {email: 'Guest'};
+  res.locals.user = req.session.user || { email: 'Guest' };
   next();
 });
 
